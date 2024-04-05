5c6dc3c3234c14a9869f3acd8fb22c19cbb1ac4e
Rijin Mk <rijin.mk9@gmail.com>
2024-04-06 01:22:11 +0400

    feat: gsap setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const container = useRef();
+
+  useGSAP(
+    () => {
+      gsap.to(".box", { x: 100 });
+    },
+    { scope: container }
+  );
+
+  return (
+    <div ref={container} className="container">
+      <div className="box"></div>
+    </div>
+  );
 }
 
 export default App;
